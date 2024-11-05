3fcb2c1d9544fc26a3428379b15fdf879d674e6c
empty-codes <morayotimothy@gmail.com>
2024-11-05 17:57:14 +0100

    Adds cors support + modifies package.json

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require("express");
 const cron = require("node-cron");
+const cors = require("cors");
 const app = express();
 
 const nyscRoutes = require("./routes/nyscRoutes");
@@ -14,6 +15,7 @@ const { checkJobSites } = require("./controllers/jobSiteController");
 
 module.exports = app;
 
+app.use(cors());
 app.use(express.json());
 //app.use(express.static(__dirname + '/public'));
 app.use("/subscribe/nysc", nyscRoutes);
